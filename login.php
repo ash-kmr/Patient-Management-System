a0e40a9544bc289466136475e34146c5c145969a
lahane-rahul <36383136+lahane-rahul@users.noreply.github.com>
2018-04-01 17:30:45 +0530

    doctor,patient checkbox , backened for verifying  updated

--- login.php
+++ login.php
@@ -1,58 +1,133 @@
+
+<?php
+
+/*Connection =.php will be included in inncludes folder*/
+include("connection.php");
+
+session_start();
+
+
+if($_SERVER["REQUEST_METHOD"] == "POST")
+	{
+		if($_POST['Login'])
+			{  
+				$username = $conn->escape_string($_POST['Email']);
+				$password = $conn->escape_string($_POST['Password']);
+                
+			//	echo "sadsads";
+				if(isset($_POST['user']) && !isset($_POST['doctor']))
+					{
+					 
+                      $sql = "select P_id , count(*) as total from auth_patient where email = '".$username."' and password = '".$password."' ";
+					  $result = $conn->query($sql);
+					  $row = $result->fetch_assoc();
+     
+					  if( $row['total']!=0)
+						{ 
+                           $_SESSION['login_user'] = $username;   	                  
+					        echo  $username." signed in "; 
+ 		                    header("location: welcome.php");
+						}
+					 else
+					   {
+ 						echo "<script type='text/javascript'>alert('Invalid Username/Password ');</script>";  
+					   }	
+					}
+					
+				else if(!isset($_POST['user']) && isset($_POST['doctor']))
+					{
+                      $sql = "select doctor_id , count(*) as total from auth_doctor where email = '".$username."' and password = '".$password."' ";
+					  $result = $conn->query($sql);
+					  $row = $result->fetch_assoc();
+     
+					  if( $row['total']!=0)
+						{ 
+                           $_SESSION['login_user'] = $username;   	                  
+					        echo  $username." Doctor signed in "; 
+ 		                    header("location: welcome.php");
+						}
+					 else
+					   {
+ 						echo "<script type='text/javascript'>alert('Invalid Username/Password ');</script>";  
+					   }	
+					}
+					
+					echo "<script type='text/javascript'>alert('Invalid Username/Password ');</script>";  
+			}
+			 else
+			 {
+				$error = "Your Login Name invalid";
+				echo "<script type='text/javascript'>alert('$error');</script>";  
+			 }
+	}
+	else{
+		echo "not post";
+	}
+  
+?>
+
+
+
+
+
 <!DOCTYPE html>
-<html lang="en">
-<head>
-  <title>Bootstrap Example</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <link rel="stylesheet" type="text/css" href="css/style.css">
-  <link href='https://fonts.googleapis.com/css?family=Balthazar' rel='stylesheet'>
-  <link rel="stylesheet" type="text/css" href="css/JiSlider.css">
-  <link href='https://fonts.googleapis.com/css?family=Stalinist One' rel='stylesheet'>
-  <script src = "js/JiSlider.js"></script>
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-  <link href='https://fonts.googleapis.com/css?family=Autour One' rel='stylesheet'>
-  <script src = "js/jquery.min.js"></script>
-</head>
-<body>
-<div class = "mynav">
-	 <nav class="navbar navbar-inverse navbar-fix navbar-fixed-top">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" style = "color:black; font-size:1.5em" href="#">WebSiteName</a>
-    </div>
-    <div class="collapse navbar-collapse" id="myNavbar">
-      <ul class="nav navbar-nav navbar-right">
-        <li><a href="#">Home</a></li>
-        <li><a href="#">Page 1</a></li>
-        <li><a href="#">Page 2</a></li>
-        <li><a href="#">Page 3</a></li>
-      </ul>
-      <ul class="nav navbar-nav navbar-right">
-        <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
-        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
-      </ul>
-      </div>
-      </div>
-      </nav>
-<div class = "separator container-fluid"> nrwewma</div>
-</div><div class = "container" style="padding-top: 10%;padding-right: 10%;padding-left: 10%">
-  <div class = "col-sm-12" style="background-color:black; padding-left: 15%; padding-right: 15% ">
-  <br>
-  <h1 style="color:white; font-family: Balthazar; text-align: center; font-size: 
-  4em">Login</h1><br><br>
-  <label for="fname" style="font-size: 1.5em; font-family: Balthazar">User ID</label>
-  <input type="text" id="fname" name="fname" placeholder="User ID">
-  <label for="lname" style="font-size: 1.5em;margin-top: 5%; font-family: Balthazar">Password</label>
-  <input type="text" id="lname" name="lname" placeholder="Password">
-  <div style="text-align: center; margin-top: 10%;margin-bottom: 10%"><button type="submit" class = "buttonlogin button3">Login</button></div>
-</div>
-</div>
-</body>
-</html>
+<html>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <head>
+	    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+		<link rel="stylesheet" href="CSS-login_sign_up/login.css">
+		<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+		<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+	</head>
+	  
+	 <body>
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-4">
+					<div class="card card-container">
+						<img id="profile-img" class="profile-img-card" src="//ssl.gstatic.com/accounts/ui/avatar_2x.png" />
+							<p id="profile-name" class="profile-name-card"></p>
+				
+								<form form action="" method = "post" class="form-signin">
+									<span id="reauth-email" class="reauth-email"></span>
+										<label>UserName  :</label>
+										<input type="email" id="inputEmail" name="Email" class="form-control" placeholder="Email address" required autofocus>
+											<script>
+												var email = document.getElementById("inputEmail").value;
+												if(email.length > 0)
+												alert(email);
+											</script>
+				 
+									<label>Password  :</label>
+										<input type="password" id="inputPassword" name="Password" class="form-control" placeholder="Password" required>
+											<div id="remember" class="checkbox">
+												<label>
+													<input type="checkbox" value="Patient" name="user" value="1"> Patient</br>
+													<input type="checkbox" value="doctor" name="doctor" value ="2"> Doctor</br>
+													<input type="checkbox" value="remember-me"> Remember me
+												</label>
+											</div>
+                
+								 <input type = "submit" name ="Login" value = " Login "   class="btn btn-lg btn-primary btn-block btn-signin" /><br />
+							
+								</form><!-- /form -->
+								<!-- Display signup page link in parralel-->
+			
+								<div style="display : inline-block">
+									<a href="#" class="forgot-password"> Forgot the password?</a>
+								</div>
+						
+								<div style = "display : inline-block;margin-left : 75px">
+									<a href="http://localhost/LOG/sign_up.php" class="forgot-password"> Register</a>
+								</div >
+
+					</div><!-- /card-container -->
+				</div><!-- /Col -md -6 -->
+		
+			</div><!-- /row -->
+		</div><!-- /container -->
+	  
+	 </body>
+	  
+	  
+ </html>
